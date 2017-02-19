8898214af8a6729d793c8a23bc122b504645116a
Joel Wilsson <joel.wilsson@gmail.com>
2017-02-19 19:25:57 +0100

    Move Spark dependencies to :profiles :provided, bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.wjoel/spark-streaming-wikiedits "0.1.1-SNAPSHOT"
+(defproject com.wjoel/spark-streaming-wikiedits "0.1.2-SNAPSHOT"
   :description "Spark Streaming receiver for Wikipedia edits"
   :url "https://github.com/wjoel/spark-streaming-wikiedits"
   :license {:name "MIT License"
@@ -8,11 +8,11 @@
   :prep-tasks [["compile" "com.wjoel.spark.streaming.wikiedits.edit-event"] "javac" "compile"]
   :aot :all
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.scala-lang/scala-library "2.11.8" :scope "provided"]
-                 [org.apache.spark/spark-core_2.11 "2.0.2" :scope "provided"]
-                 [org.apache.spark/spark-streaming_2.11 "2.0.2" :scope "provided"]
                  [org.schwering/irclib "1.10"]
                  [com.wjoel/clj-bean "0.1.0"]]
+  :profiles {:provided {:dependencies [[org.scala-lang/scala-library "2.11.8"]
+                                       [org.apache.spark/spark-core_2.11 "2.0.2"]
+                                       [org.apache.spark/spark-streaming_2.11 "2.0.2"]]}}
   :deploy-repositories {"releases" {:url "https://oss.sonatype.org/service/local/staging/deploy/maven2/"
                                     :creds :gpg}
                         "snapshots" {:url "https://oss.sonatype.org/content/repositories/snapshots/"
